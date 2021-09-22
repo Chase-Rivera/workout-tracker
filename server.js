70f8e39044de9eb478feb594871d0fff6df1150a
Chase Rivera <achaserivera@gmail.com>
2021-09-22 12:09:56 -0700

    finished building out server.js for functionaility

--- server.js
+++ server.js
@@ -7,3 +7,20 @@ const routes = require("./controllers");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
+app.use (express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.use(routes);
+
+app.listen(PORT, () => {console.log(`App running on port ${PORT}!`)});
